6a49b3f5c854ebc9b8306ddbc89a1c1c23503f87
Nathaniel Struselis <nathaniel@kali.local>
2022-04-21 19:15:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is my solution to the www.hackthissite.org Realistic Mission 6.
+# This mission is fairly simple so I have not bothered to make this work well. It only needs to work once....
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
